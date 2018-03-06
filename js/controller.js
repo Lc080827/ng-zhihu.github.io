7cfb06b538a31b1b0fc3649730b6f67681da9b2f
LC080827 <741332627@qq.com>
2018-03-06 11:06:13 +0800

    知乎数据接口处理及路由跳转及图片过滤器处理

--- js/controller.js
+++ js/controller.js
@@ -1,38 +1,23 @@
-var app = angular.module('myapp',['ngRoute']);
+var app = angular.module('myapp',['ngRoute','ngSanitize']);
 app.config(['$routeProvider',function($routeProvider){
     $routeProvider
     .when('/',{
         templateUrl:'html/homepage.html',
         controller:'homepageController'
+    }).when('/detail/:id',{
+        templateUrl:'html/detail.html',
+        controller:'detailController'
     });
 }]);
 
-//组件复用
-app.component("lunbo",{
-    templateUrl:'component/lunbo.html',
-    controller:'lunboController'
-});
-
 //主页面控制器
-app.controller('homepageController',function($scope,$http,$routeParams){
-    $scope.id = $routeParams.id;
+app.controller('homepageController',function($scope,$http){
     $http({
        method:'GET',
        url:'http://192.168.10.141:8888/news-at/api/4/news/latest',
-        params:{
-            columnId:$scope.id
-        }
     }).then(function(data){
         $scope.newsList = data.data.stories;
-        for(var i=0;i<$scope.newsList.length;i++){
-            $scope.newsList[i].url = 'http://192.168.10.141:8888/news-at/api/4/news/latest?id='+$scope.newsList[i].id;
-        }
-        // 修改图片链接 直接访问知乎图片显示403 需要缓存处理
-        $scope.attachImageUrl=function(srcUrl) {
-            if (srcUrl !== undefined) {
-                return srcUrl.replace(/http\w{0,1}:\/\/p/g, 'https://images.weserv.nl/?url=p');
-            }
-        }
+        console.log($scope.newsList);
     });
 });
 
@@ -40,24 +25,12 @@ app.controller('homepageController',function($scope,$http,$routeParams){
 app.controller("lunboController",function($scope,$http){
    $http({
       method:'GET',
-      url:'http://192.168.10.141:8888/news-at/api/4/news/latest',
-      params:{
-          columnId:$scope.id
-      }
+      url:'http://192.168.10.141:8888/news-at/api/4/news/latest'
    }).then(function(data){
        $scope.picList = data.data.top_stories;
        console.log($scope.picList);
-       // 修改图片链接 直接访问知乎图片显示403 需要缓存处理
-       $scope.attachImageUrl=function(srcUrl) {
-           if (srcUrl !== undefined) {
-               return srcUrl.replace(/http\w{0,1}:\/\/p/g, 'https://images.weserv.nl/?url=p');
-           }
-       }
-       // for(var i=0;i<$scope.picList.length;i++){
-       //     $scope.picList[i].url = 'json/serviceCenter.json?docId='+$scope.picList[i].id;
-       // }
    });
-    //轮播图
+   //图片轮播
     setTimeout(function(){$('#sliderBox').bxSlider({
         mode:'horizontal', //默认的是水平
         displaySlideQty:1,//显示li的个数
@@ -66,6 +39,38 @@ app.controller("lunboController",function($scope,$http){
         auto: true,
         controls: false,//隐藏左右按钮
         pager:true,
-        pause:2000,
-    });},0)
+        pause:3000,
+    });},500)
+});
+
+//详细页控制器
+app.controller("detailController",function($scope,$http,$routeParams){
+    $http({
+        method:'GET',
+        url:'http://192.168.10.141:8888/news-at/api/4/news/'+$routeParams.id
+    }).then(function(data){
+         $scope.detail = data.data;
+         console.log($scope.detail);
+    });
+    $scope.back = function(){
+        history.go(-1);
+    }
+});
+
+//自定义图片缓存过滤器
+app.filter("attachImageUr",function(){
+    // 修改图片链接 直接访问知乎图片显示403 需要缓存处理
+    return function(srcUrl){
+        //防止变量为空的情况
+        if( srcUrl !== undefined ){
+            return srcUrl.replace(/http\w{0,1}:\/\/p/g, 'https://images.weserv.nl/?url=p');
+        }
+    }
+});
+
+
+//组件复用
+app.component("lunbo",{
+    templateUrl:'component/lunbo.html',
+    controller:'lunboController'
 });
